787840cf1dcc2b087eb40da1f23ef021152e71ea
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-04 17:09:29 -0700

    Initial commit with all files.

---
+++ src/components/Board/Board.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Board.css';
+import Tile from '../Tile/Tile.js';
+
+const Board = (props) => {
+
+  const gridConfig = {
+    gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
+    gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
+  }
+
+  const tileMapping = props.tiles.map((tile) => ((
+    <Tile {...tile}/>))
+  );
+
+  return (
+    <div className='Board' style={gridConfig}>
+      {tileMapping}
+    </div>
+  )
+}
+
+export default Board
---
+++ src/components/OptionsPanel/OptionsPanel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Button from '../Button';
+import TileSelector from '../TileSelector';
+
+const OptionsPanel = (props) => (
+  <div>
+    <TileSelector
+      numTiles={props.numTiles}
+    />
+    <Button
+      playing={props.playing}
+      startGame={props.startGame}
+    />
+  </div>
+);
+
+
+
+export default OptionsPanel;
