119a5ae3ab2378615160d107eaf7fcb5cf00e6f9
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-07 23:07:50 -0700

    Fix scope of handleTileClicked this.setState function.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,33 +34,33 @@ class App extends Component {
       let toBeCleared = state.toBeCleared;
       const selectedTileIndex = indexOfSelected( tiles, id, color );
       let previousTileIndex = state.previousTileIndex;
-    });
 
-    if (toBeCleared !== null) {
-      tiles[toBeCleared[0]].selected = false;
-      tiles[toBeCleared[1]].selected = false;
-      toBeCleared = null;
-    }
-
-    tiles[selectedTileIndex].selected = true;
-
-    if (previousTileIndex !== null) {
-      let previousTile = tiles[previousTileIndex];
-      let selectedTile = tiles[selectedTileIndex];
-      if (previousTile.id !== selectedTile.id && previousTile.color === color) {
-        selectedTile.matched = true;
-        previousTile.matched = true;
-        previousTileIndex = null;
-      } else {
-        toBeCleared = [previousTileIndex, selectedTileIndex];
-        previousTileIndex = null;
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
       }
-    } else {
-      previousTileIndex = selectedTileIndex;
-    }
+
+      tiles[selectedTileIndex].selected = true;
+
+      if (previousTileIndex !== null) {
+        let previousTile = tiles[previousTileIndex];
+        let selectedTile = tiles[selectedTileIndex];
+        if (previousTile.id !== selectedTile.id && previousTile.color === color) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else {
+        previousTileIndex = selectedTileIndex;
+      }
 
 
-    return { toBeCleared, tiles, previousTileIndex };
+      return { toBeCleared, tiles, previousTileIndex };
+    });
   };
 
   render() {
