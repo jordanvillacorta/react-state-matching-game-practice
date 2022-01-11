dfbe22c5a9c723b0e54bb27ff381c1fe70ae9aba
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-10 23:26:20 -0700

    Create a callback to use on mouseenter.

--- src/hooks.js
+++ src/hooks.js
@@ -4,10 +4,14 @@ function useHover() {
   const ref = useRef();
   const [hovered, setHovered] = useState(false);
 
+  function enter() {
+    setHovered(true);
+  }
+
   useEffect(() => {
     const refCopy = ref;
-    refCopy.current.addEventListener('mouseenter');
-    refCopy.current.addEventListener('mouseleave');
+    refCopy.current.addEventListener('mouseenter', enter);
+    refCopy.current.addEventListener('mouseleave', () => {});
     return () => {
 
     };
