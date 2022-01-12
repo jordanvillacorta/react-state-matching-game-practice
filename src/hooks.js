371183ed648af5f4242af5221ca407d9443039c4
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-11 21:58:15 -0700

    Create a callback to use on mouseleave.

--- src/hooks.js
+++ src/hooks.js
@@ -5,11 +5,12 @@ function useHover() {
   const [hovered, setHovered] = useState(false);
 
   const enter = () => setHovered(true);
+  const leave = () => setHovered(false);
 
   useEffect(() => {
     const refCopy = ref;
     refCopy.current.addEventListener('mouseenter', enter);
-    refCopy.current.addEventListener('mouseleave', () => {});
+    refCopy.current.addEventListener('mouseleave', leave);
     return () => {
 
     };
