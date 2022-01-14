f65b163890347407093660593f10abc71db19618
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-13 23:18:24 -0700

    Import Game Context, create anonymous function for Button.

--- refactor/components/Button/Button.js
+++ refactor/components/Button/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Button.css';
+import GameContext from '../../GameContext.js';
 
-const Button = (props) => (
-  <button onClick={props.startGame}>
-    {props.playing ? 'reset' : 'start'}
-  </button>
-  )
+const Button = (props) => {
+  {
+    ({ playing, startGame }) =>
+      <button onClick={props.startGame}>
+        {props.playing ? 'reset' : 'start'}
+      </button>
+  }
+}
 
 export default Button;
