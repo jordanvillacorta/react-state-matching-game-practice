2088b73649412fb7d29cd8d436326b33f8a31608
Jordan Villacorta <jvillacortadla@gmail.com>
2022-01-18 22:50:03 -0700

    Format code to standard Prettier format.

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import './Tile.css'
+import React from "react";
+import "./Tile.css";
 
 const Tile = (props) => {
-
-  const dynamicColor = props.selected || props.matched ? {backgroundColor: props.color} : null;
+  const dynamicColor =
+    props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className='Tile' style={dynamicColor} onClick={() => {props.handleTileClicked(props.id, props.color)}}>
+    <div
+      className="Tile"
+      style={dynamicColor}
+      onClick={() => {
+        props.handleTileClicked(props.id, props.color);
+      }}
+    >
       {props.selected || props.matched ? <props.svg /> : null}
     </div>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
