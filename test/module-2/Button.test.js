392676350e4ac41a57d23094848d384db7430841
Jordan Villacorta <jvillacortadla@gmail.com>
2022-02-01 20:40:53 -0700

    Format with Prettier.

--- test/module-2/Button.test.js
+++ test/module-2/Button.test.js
@@ -1,40 +1,43 @@
-import React from 'react'
+import React from "react";
 
-import Button from '../../src/components/Button'
+import Button from "../../src/components/Button";
 
-import { shallow } from 'enzyme'
+import { shallow } from "enzyme";
 
-describe('Button', () => {
-  let wrapper = shallow(<Button playing={false} startGame={() => {}}/>)
+describe("Button", () => {
+  let wrapper = shallow(<Button playing={false} startGame={() => {}} />);
 
-  it('Registers a click handler to the button @register-start-game-on-click', () => {
-    const buttonProps = wrapper.find('button').props()
+  it("Registers a click handler to the button @register-start-game-on-click", () => {
+    const buttonProps = wrapper.find("button").props();
 
-    expect(buttonProps.onClick, 'Did you pass the startGame prop to the onClick handler?').toBeInstanceOf(Function)
-  })
+    expect(
+      buttonProps.onClick,
+      "Did you pass the startGame prop to the onClick handler?"
+    ).toBeInstanceOf(Function);
+  });
 
-    it('Conditionally renders text @conditionally-render-text', () => {
-      let buttonChild = wrapper.find('button').childAt(0)
+  it("Conditionally renders text @conditionally-render-text", () => {
+    let buttonChild = wrapper.find("button").childAt(0);
 
-      try {
-        buttonChild = buttonChild.text()
-      } catch(error) {
+    try {
+      buttonChild = buttonChild.text();
+    } catch (error) {}
 
-      }
+    expect(
+      buttonChild,
+      "Did you pass the numTiles prop to TileSelector?"
+    ).toEqual("start");
 
-      expect(buttonChild, 'Did you pass the numTiles prop to TileSelector?').toEqual('start')
+    wrapper = shallow(<Button playing={true} startGame={() => {}} />);
+    buttonChild = wrapper.find("button").childAt(0);
 
-      wrapper = shallow(<Button playing={true} startGame={() => {}}/>)
-      buttonChild = wrapper.find('button').childAt(0)
+    try {
+      buttonChild = buttonChild.text();
+    } catch (error) {}
 
-      try {
-        buttonChild = buttonChild.text()
-      } catch(error) {
-
-      }
-
-      expect(buttonChild, 'Did you pass the numTiles prop to TileSelector?').toEqual('reset')
-    })
-
-})
-
+    expect(
+      buttonChild,
+      "Did you pass the numTiles prop to TileSelector?"
+    ).toEqual("reset");
+  });
+});
